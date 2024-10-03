e0e20d3a662b17623ac74cbbcdc7b4b02f5bbb1d
VIVEK PATEL <patelvk2000@gmail.com>
2024-10-03 02:18:53 -0400

    Fix: header

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -4,52 +4,50 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <header className="bg-cozey-Sky-Bold hidden lg:flex text-cozey-Fog-Soft text-xs font-semibold py-2">
-        <div className="mx-auto px-4 sm:px-6 lg:px-20 flex items-center justify-between">
-          <div className="flex flex-row gap-3">
-            <p>Designed in North America</p>
-            <p>|</p>
-            <p>Fast & Free Shipping</p>
-          </div>
-          <div className="flex flex-row items-center gap-8">
-            <a href="#reviews" className="hover:text-gray-900">
-              Reviews
-            </a>
-            <a href="#free-swatches" className="hover:text-gray-900">
-              Free Swatches
-            </a>
-            <a href="#financing" className="hover:text-gray-900">
-              Financing
-            </a>
-            <a href="#support" className="hover:text-gray-900">
-              Support
-            </a>
-            <a href="#contact-us" className="hover:text-gray-900">
-              Contact Us
-            </a>
-            <a href="#our-locations" className="hover:text-gray-900">
-              Our Locations
-            </a>
+      <header className="bg-cozey-Sky-Bold hidden lg:flex mx-auto px-4 sm:px-6 lg:px-20 items-center justify-between text-cozey-Fog-Soft text-xs font-semibold py-2">
+        <div className="flex flex-row gap-3">
+          <p>Designed in North America</p>
+          <p>|</p>
+          <p>Fast & Free Shipping</p>
+        </div>
+        <div className="flex flex-row items-center gap-8">
+          <a href="#reviews" className="hover:text-gray-900">
+            Reviews
+          </a>
+          <a href="#free-swatches" className="hover:text-gray-900">
+            Free Swatches
+          </a>
+          <a href="#financing" className="hover:text-gray-900">
+            Financing
+          </a>
+          <a href="#support" className="hover:text-gray-900">
+            Support
+          </a>
+          <a href="#contact-us" className="hover:text-gray-900">
+            Contact Us
+          </a>
+          <a href="#our-locations" className="hover:text-gray-900">
+            Our Locations
+          </a>
 
-            <div className="flex flex-row gap-1.5">
-              <span>EN</span>
-              <span className="flex flex-row gap-0.5">
-                <Image
-                  src="/USA.svg"
-                  className="rounded-full"
-                  alt="usa"
-                  width={16}
-                  height={16}
-                />
-                <Image
-                  src="/Chevron-down.svg"
-                  className="rounded-full"
-                  alt="canada"
-                  width={12}
-                  height={12}
-                />
-              </span>
-            </div>
+          <div className="flex flex-row gap-1.5">
+            <span>EN</span>
+            <span className="flex flex-row gap-0.5">
+              <Image
+                src="/USA.svg"
+                className="rounded-full"
+                alt="usa"
+                width={16}
+                height={16}
+              />
+              <Image
+                src="/Chevron-down.svg"
+                className="rounded-full"
+                alt="canada"
+                width={12}
+                height={12}
+              />
+            </span>
           </div>
         </div>
       </header>
